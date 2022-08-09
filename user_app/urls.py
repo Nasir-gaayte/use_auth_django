9e45190885d21adbf219ae13da38a9e81c513fc0
nasir-gaayte <slowboyz12@gmail.com>
2022-08-09 19:41:31 +0300

    finshed login and logout

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'user_app'
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('register/',views.register_req,name='register'),
+    path('login/',views.login_req,name='login'),
+    path('logout/',views.logout_req,name='logout'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,4 +1,4 @@
-from configparser import LegacyInterpolation
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render,redirect
 from .forms import NewUserForm
 from django.contrib.auth import login,logout,authenticate
@@ -23,3 +23,28 @@ def register_req(request):
         messages.error(request,"NO ......")
     form = NewUserForm()
     return render(request,'user_app/register.html',{'register_form':form})
+
+def login_req(request):
+    if request.method == "POST":
+        form =AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user= authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                messages.info(request,f"Welcome{username}")
+                return redirect('user_app:home')
+            else:
+                messages.error(request,"Invalid username or password")
+        else:
+            messages.error(request,"sorry????")
+    form = AuthenticationForm()
+    return render(request,'user_app/login.html',{'login_form':form})                 
+
+
+
+def logout_req(request):
+    logout(request)
+    messages.info(request,"googby see you soon")
+    return redirect('user_app:home')
