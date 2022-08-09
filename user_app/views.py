418b445722b5825f78b0f8c9043b39c69c6ad7cb
nasir-gaayte <slowboyz12@gmail.com>
2022-08-09 15:14:45 +0300

    add the forms

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from configparser import LegacyInterpolation
+from django.shortcuts import render,redirect
+from .forms import NewUserForm
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+
 
 # Create your views here.
+
+def home(request):
+    return render(request,'user_app/home.html')
+
+
+
+def register_req(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request,"successfull")
+            return redirect('home')
+        messages.error(request,"NO ......")
+    form = NewUserForm()
+    return render(request,'user_app/register.html',{'register_form':form})
